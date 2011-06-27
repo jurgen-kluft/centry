73ec47b872797391e64f310ca8bfb8bb26b79cc0
Jurgen Kluft <jurgen@virtuos.com.cn>
2011-06-27 18:38:39 +0800

    *: testing AppMain

---
+++ source/test/cpp/test_appmain.cpp
@@ -0,0 +1,31 @@
+#include "xbase\x_types.h"
+#include "xbase\x_allocator.h"
+
+#include "xunittest\xunittest.h"
+
+using namespace xcore;
+
+extern xcore::s32 AppMain(xcore::s32 argc, const char** argv);
+
+UNITTEST_SUITE_BEGIN(appmain)
+{
+    UNITTEST_FIXTURE(main)
+    {
+        UNITTEST_FIXTURE_SETUP()
+		{
+		}
+
+        UNITTEST_FIXTURE_TEARDOWN()
+		{
+		}
+
+        UNITTEST_TEST(run)
+        {
+			const char* argv[] = { "" };
+			xcore::s32 result = AppMain(0, (const char**)argv);
+			CHECK_EQUAL(0, result);
+        }
+
+	}
+}
+UNITTEST_SUITE_END
