6c411c56ae3f4b81313fc326cab7c962cbc0bd4c
Jurgen Kluft <jurgen@virtuosgames.com>
2023-05-08 16:39:42 +0800

    * cbase -> ccore

--- source/test/cpp/test_main.cpp
+++ source/test/cpp/test_main.cpp
@@ -1,7 +1,7 @@
-#include "cbase/c_target.h"
+#include "ccore/c_target.h"
 #include "cbase/c_allocator.h"
 #include "cbase/c_base.h"
-#include "cbase/c_debug.h"
+#include "ccore/c_debug.h"
 #include "cbase/c_console.h"
 #include "cbase/c_context.h"
 
